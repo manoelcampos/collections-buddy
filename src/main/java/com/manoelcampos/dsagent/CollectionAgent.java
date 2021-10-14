c79b12f251ea2e7814fe0ef4b15c966f9acb4a09
Manoel Campos <manoelcampos@gmail.com>
2021-10-14 16:49:19 -0300

    CollectionAgent update to enable tracing method calls on JDK classes.

--- src/main/java/com/manoelcampos/dsagent/CollectionAgent.java
+++ src/main/java/com/manoelcampos/dsagent/CollectionAgent.java
@@ -3,45 +3,36 @@ package com.manoelcampos.dsagent;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.type.TypeDescription.ForLoadedType;
-import net.bytebuddy.dynamic.ClassFileLocator.ForClassLoader;
-import net.bytebuddy.dynamic.loading.ClassInjector.UsingInstrumentation;
-import net.bytebuddy.dynamic.loading.ClassInjector.UsingInstrumentation.Target;
-import net.bytebuddy.matcher.ElementMatchers;
 
-import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
-import java.nio.file.Files;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Random;
 
-import java.util.ArrayList;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class CollectionAgent {
     public static void premain(final String agentArgs, final Instrumentation inst) throws IOException{
-        final File temp = Files.createTempDirectory("temp").toFile();
-
-        final var typesMap = Map.of(new ForLoadedType(CollectionAdvices.class), ForClassLoader.read(CollectionAdvices.class));
-        UsingInstrumentation
-                .of(temp, Target.BOOTSTRAP, inst)
-                .inject(typesMap);
-
         new AgentBuilder.Default()
-                .ignore(ElementMatchers.nameStartsWith("net.bytebuddy."))
-                .with(new AgentBuilder.InjectionStrategy.UsingInstrumentation(inst, temp))
-                .type(ElementMatchers.nameEndsWith(".ArrayList"))
-                .transform((builder, typeDescription, classLoader, module) ->
-                        builder.visit(Advice.to(CollectionAdvices.class)
-                                            .on(ElementMatchers.isMethod())))
+                // by default, JVM classes are not instrumented
+                .ignore(nameStartsWith("net.bytebuddy."))
+                .disableClassFormatChanges()
+                .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+                .type(nameEndsWith("ArrayList"))
+                .transform((builder, type, loader, module) -> builder
+                        .visit(Advice
+                                .to(CollectionAdvices.class)
+                                .on(isMethod())
+                        ))
                 .installOn(inst);
     }
 
     public static void main(String[] args) throws IOException{
+        System.out.println("Starting " + CollectionAgent.class.getName());
+
         final Instrumentation instrumentation = ByteBuddyAgent.install();
         premain("", instrumentation);
 
-        System.out.println("Starting " + CollectionAgent.class.getName());
         final  var arrayList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             arrayList.add(i);
